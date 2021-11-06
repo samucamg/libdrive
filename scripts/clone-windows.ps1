d550abddad925b74cb178e593da138f4d1ff0b3e
Samuel Santos <samucamg@gmail.com>
2021-11-06 18:21:40 -0300

    Update clone-windows.ps1

--- scripts/clone-windows.ps1
+++ scripts/clone-windows.ps1
@@ -1,11 +1,11 @@
 cd ..
 
-git clone https://github.com/libDrive/cloudflare.git
-git clone https://github.com/libDrive/config.git
-git clone https://github.com/libDrive/heroku.git
-git clone https://github.com/libDrive/libdrive.github.io.git
-git clone https://github.com/libDrive/server.git
-git clone https://github.com/libDrive/web.git
-git clone https://github.com/libDrive/webhooks.git
+git clone https://github.com/samucamg/libdrive-cloudflare.git
+git clone https://github.com/samucamg/libdrive-config.git
+git clone https://github.com/samucamg/libdrive-heroku.git
+git clone https://github.com/samucamg/libdrive.github.io.git
+git clone https://github.com/samucamg/libDrive-server.git
+git clone https://github.com/samucamg/libDrive-web.git
+git clone https://github.com/samucamg/libdrive-webhooks.git
 
 cd libDrive
